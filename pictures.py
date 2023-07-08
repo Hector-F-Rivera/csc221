55aa896a26fd72e5221d95c069b28a7a9ed9f90f
Hector <hr9457768@gmail.com>
2023-07-08 18:08:55 -0400

    Fix Eyes

--- pictures.py
+++ pictures.py
@@ -3,8 +3,8 @@ begin_graphics()
 Line((266, 194), (364, 194))
 Circle((320,240), 220)
 Line((266, 194), (320,286))
-for r in 285,315:
-    Circle((r, 210), 5)
+for r in 239,401:
+    Circle((r, 286), 28)
 Arc((300, 200), 30, 280, 320)
 update_when('key_pressed')
 end_graphics()
