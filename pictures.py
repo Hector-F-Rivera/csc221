5d3fb3c3f135a9d43f67dafd8bcda94e1cf5996c
Hector <hr9457768@gmail.com>
2023-07-12 10:11:12 -0400

    Fix ver

--- pictures.py
+++ pictures.py
@@ -1,10 +1,6 @@
 from gasp import *
-def faced(x,y):
-    begin_graphics()
-    Line((x-54, y-46), (x+54, y-46))
-    Circle((x,y), 220,color="blue",filled=True)
-    Line((x-54, y-46), (x,y+46))
-    def eyed(x,y):
+
+def eyed(x, y):
         for e in x-81,x+81:
             Line((e-7, y-6), (e+7, y-6))
             Circle((e,y), 28, color="red",filled=True)
@@ -13,22 +9,22 @@ def faced(x,y):
                 Circle((r, y+3), 3, color="blue",filled=True)
             Arc((e, y), 19, 280, 320)
             for r in e-10, e+10:
-                Arc((r,y+6,),5,25,155)
+                Arc((r, y+6,),5,25,155)
+
+def faced(x, y):
+    Line((x-54, y-46), (x+54, y-46))
+    Circle((x,y), 220)
+    Line((x-54, y-46), (x,y+46))
     eyed(x,y+46)
     Arc((x, y), 150, 280, 320)
     for r in x-81, x+81:
         Arc((r,y+46,),40,25,155)
-q = "Enter the X value."
+
+q = "Enter the X value: "
 x = int(input(q))
-qt = "Enter the y value"
+qt = "Enter the y value: "
 y = int(input(qt))
-faced(x,y)
-def moan():
-    print("Python is useless")
-    print("And so are these worksheets.")
-moan()
-def Square(x):
-    print(x, 'times', x, 'is', x * x)
-Square(3)
+begin_graphics()
+faced(x, y) 
 update_when('key_pressed')
 end_graphics()
